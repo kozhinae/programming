ba42c9eec55cf0a9a6fffe5fc06b1d573d9d6401
Екатерина Кожина <katerinabonia@mail.ru>
2025-05-03 22:20:07 +0300

    Create notification.go

---
+++ Go/CI_notification_generator/notification.go
@@ -0,0 +1,42 @@
+//go:build !change
+
+package ciletters
+
+type Notification struct {
+	Project  GitlabProject
+	Branch   string
+	Commit   Commit
+	Pipeline Pipeline
+}
+
+type GitlabProject struct {
+	GroupID string
+	ID      string
+}
+
+type Commit struct {
+	Hash    string
+	Message string
+	Author  string
+}
+
+type PipelineStatus string
+
+const (
+	PipelineStatusOK     PipelineStatus = "ok"
+	PipelineStatusFailed PipelineStatus = "failed"
+)
+
+type Pipeline struct {
+	Status      PipelineStatus
+	ID          int64
+	TriggeredBy string
+	FailedJobs  []Job
+}
+
+type Job struct {
+	ID        int64
+	Name      string
+	Stage     string
+	RunnerLog string
+}
